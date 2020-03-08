e6a069cb235e72dd8bb25f487076263c721975db
VDNguyen <vdnguyen3012@gmail.com>
2020-03-08 10:06:29 -0400

    update features

--- pages/js/custom.js
+++ pages/js/custom.js
@@ -20,3 +20,72 @@
 //               '</div>';
 //   // $("#playlists").preappend(html);
 // })
+
+// Setup the player
+const player = new Plyr('#player', { controls: [
+  'play-large', // The large play button in the center
+  'restart', // Restart playback
+  'rewind', // Rewind by the seek time (default 10 seconds)
+  'play', // Play/pause playback
+  'fast-forward', // Fast forward by the seek time (default 10 seconds)
+  'progress', // The progress bar and scrubber for playback and buffering
+  'current-time', // The current time of playback
+  // 'duration', // The full duration of the media
+  'mute', // Toggle mute
+  'volume', // Volume control
+  'captions', // Toggle captions
+  'settings', // Settings menu
+  'pip', // Picture-in-picture (currently Safari only)
+  'airplay', // Airplay (currently Safari only)
+  'download', // Show a download button with a link to either the current source or a custom URL you specify in your options
+  'fullscreen', // Toggle fullscreen
+] });
+
+$('.page-container').on('click', '.btn-play', function(){
+  $('.btn-pause').each(function(){
+    $(this).attr('data-is-played', 0)
+    $(this).find('i').removeClass('fa-pause').addClass('fa-play-circle');
+    $(this).removeClass('btn-pause').addClass('btn-play');
+  })
+  
+
+  var isPlayed = $(this).attr('data-is-played');
+  if(isPlayed == false) {
+    var song = $(this).data('song');
+    $(this).attr('data-is-played', 1)
+    $('.btn-pause').each(function(){
+      $(this).attr('data-is-played', 0)
+    })
+
+    player.source = {
+        type: 'audio',
+        title: 'Example title',
+        sources: [
+            {
+                src: 'assets/audios/' + song + '.mp3',
+                type: 'audio/mp3',
+            }
+        ],
+    };
+  }
+
+  $(this).find('i').removeClass('fa-play-circle').addClass('fa-pause');
+  $(this).removeClass('btn-play').addClass('btn-pause');
+
+  player.play();
+})
+
+$('.page-container').on('click', '.btn-pause', function(){
+  $(this).find('i').removeClass('fa-pause').addClass('fa-play-circle');
+  $(this).removeClass('btn-pause').addClass('btn-play');
+  player.pause();
+})
+
+$('.btn-like').on('click', function(){
+  if($(this).find('i').hasClass('fa-heart-o')){
+    $(this).find('i').removeClass('fa-heart-o').addClass('fa-heart');
+  }
+  else{
+    $(this).find('i').removeClass('fa-heart').addClass('fa-heart-o');
+  }
+})
